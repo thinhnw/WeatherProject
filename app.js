8f814b67b18f009c5e7bd7a8614079ed8f405caa
thinhnw <thinhnv0412@gmail.com>
2020-10-14 21:39:55 +0700

    Set port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const https = require("https");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
