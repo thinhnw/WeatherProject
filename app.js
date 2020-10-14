cf51f8a9577b98aaa9b194c9cf748a34c5fbc5d5
thinhnw <thinhnv0412@gmail.com>
2020-10-14 21:22:44 +0700

    Take user's input and show result

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
 
+	res.sendFile(__dirname + "/index.html");
+	
+});
+
+app.post("/", function(req, res) {
+	
 	let appid = "b93dc3fee15b8979cba906243aec856c";
-	let cityName = "Hanoi";
+	let cityName = req.body.cityName;
 	let units = "metric";
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${units}&appid=${appid}`;
 	
@@ -20,7 +29,7 @@ app.get("/", function(req, res) {
 			const description = weatherData.weather[0].description;
 			const icon = weatherData.weather[0].icon; 
 
-			res.write("<h1>The temperature in Hanoi is " + temp + " degree Celcius.</h1>");
+			res.write("<h1>The temperature in" + cityName + " is " + temp + " degree Celcius.</h1>");
 			res.write("<p>The weather is currently <em>" + description + "</em></p>");
 			res.write(`<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`);
 			res.send();
